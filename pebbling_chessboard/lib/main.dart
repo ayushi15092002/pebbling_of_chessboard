9fd672ef5c5cc88942cac6280d60add04de3b7a8
Ayushi Agarwal <87222543+ayushi15092002@users.noreply.github.com>
2022-01-20 16:37:58 +0530

    chessboard and one pebble added

--- pebbling_chessboard/lib/main.dart
+++ pebbling_chessboard/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chess_board/flutter_chess_board.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,13 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  ChessBoardController controller = ChessBoardController();
 
   @override
   Widget build(BuildContext context) {
@@ -43,23 +38,110 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: SizedBox(
+          width: 400,
+          height:400,
+          child: Stack(
+            children: [
+              AspectRatio(
+                child: Image.asset(
+                  "images/orange_board.png",
+                  package: 'flutter_chess_board',
+                  fit: BoxFit.cover,
+                ),
+                aspectRatio: 1.0,
+              ),
+              AspectRatio(
+                aspectRatio: 1.0,
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 8),
+                  itemBuilder: (context, index) {
+                    var row = index ~/ 8;
+                    var column = index % 8;
+                    var boardRank = '${(7 - row) + 1}';
+                    var boardFile = '${files[column]}';
+                    return index == 56 
+                    ? Container(
+                      child: Padding(
+                        padding: EdgeInsets.all(4.0),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.red[800],
+                        ),
+                      )
+                    )
+                        : Container();
+
+                    // var squareName = '$boardFile$boardRank';
+                    // var pieceOnSquare = game.get(squareName);
+
+                    // var piece = BoardPiece(
+                    //   squareName: squareName,
+                    //   game: game,
+                    // );
+
+                    // var draggable = game.get(squareName) != null
+                    //     ? Draggable<PieceMoveData>(
+                    //   child: piece,
+                    //   feedback: piece,
+                    //   childWhenDragging: SizedBox(),
+                    //   data: PieceMoveData(
+                    //     squareName: squareName,
+                    //     pieceType:
+                    //     pieceOnSquare?.type.toUpperCase() ?? 'P',
+                    //     pieceColor: pieceOnSquare?.color ?? Color.WHITE,
+                    //   ),
+                    // )
+                    //     : Container();
+
+                    // var dragTarget =
+                    // DragTarget<PieceMoveData>(builder: (context, list, _) {
+                    //   return draggable;
+                    // }, onWillAccept: (pieceMoveData) {
+                    //   return widget.enableUserMoves ? true : false;
+                    // }, onAccept: (PieceMoveData pieceMoveData) async {
+                    //   // A way to check if move occurred.
+                    //   Color moveColor = game.turn;
+                    //
+                    //   if (pieceMoveData.pieceType == "P" &&
+                    //       ((pieceMoveData.squareName[1] == "7" &&
+                    //           squareName[1] == "8" &&
+                    //           pieceMoveData.pieceColor == Color.WHITE) ||
+                    //           (pieceMoveData.squareName[1] == "2" &&
+                    //               squareName[1] == "1" &&
+                    //               pieceMoveData.pieceColor == Color.BLACK))) {
+                    //     var val = await _promotionDialog(context);
+                    //
+                    //     if (val != null) {
+                    //       widget.controller.makeMoveWithPromotion(
+                    //         from: pieceMoveData.squareName,
+                    //         to: squareName,
+                    //         pieceToPromoteTo: val,
+                    //       );
+                    //     } else {
+                    //       return;
+                    //     }
+                    //   } else {
+                    //     widget.controller.makeMove(
+                    //       from: pieceMoveData.squareName,
+                    //       to: squareName,
+                    //     );
+                    //   }
+                    //   if (game.turn != moveColor) {
+                    //     widget.onMove?.call();
+                    //   }
+                    // });
+
+                    // return dragTarget;
+                  },
+                  itemCount: 64,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                ),
+              ),
+            ],
+          ),
+        )
       ),
     );
   }
